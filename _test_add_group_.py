189e2bd932926d1256a08eb675a8c812a4ddde3b
Jkrsh <krasheninnikova-y05@mail.ru>
2025-03-10 22:54:13 +0600

    Did all lessons from part 1

--- _test_add_group_.py
+++ _test_add_group_.py
@@ -8,70 +8,70 @@ import unittest
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         service = Service(executable_path="C:\\Windows\\SysWOW64\\geckodriver.exe")
-        self.webdriver = webdriver.Firefox()
-        self.webdriver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
-    def open_home_page(self, webdriver):
+    def open_home_page(self, wd):
         # open home page
-        webdriver.get("http://localhost/addressbook/")
+        wd.get("http://localhost/addressbook/")
 
-    def login(self, webdriver, username, password):
+    def login(self, wd, username, password):
         # login
-        webdriver.find_element(By.NAME, "user").clear()
-        webdriver.find_element(By.NAME, "user").send_keys(username)
-        webdriver.find_element(By.NAME, "pass").click()
-        webdriver.find_element(By.NAME, "pass").clear()
-        webdriver.find_element(By.NAME, "pass").send_keys(password)
-        webdriver.find_element(By.XPATH, "//input[@value='Login']").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
-    def open_groups_page(self, webdriver):
+    def open_groups_page(self, wd):
         # open groups page
-        webdriver.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.LINK_TEXT, "groups").click()
 
-    def create_group(self, webdriver, group):
+    def create_group(self, wd, group):
         # init group creation
-        webdriver.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "new").click()
         # fill group form
-        webdriver.find_element(By.NAME, "group_name").click()
-        webdriver.find_element(By.NAME, "group_name").clear()
-        webdriver.find_element(By.NAME, "group_name").send_keys(group.name)
-        webdriver.find_element(By.NAME, "group_header").click()
-        webdriver.find_element(By.NAME, "group_header").clear()
-        webdriver.find_element(By.NAME, "group_header").send_keys(group.header)
-        webdriver.find_element(By.NAME, "group_footer").click()
-        webdriver.find_element(By.NAME, "group_footer").clear()
-        webdriver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
-        webdriver.find_element(By.NAME, "submit").click()
+        wd.find_element(By.NAME, "submit").click()
 
-    def return_to_group_page(self, webdriver):
+    def return_to_group_page(self, wd):
         # return to groups page
-        webdriver.find_element(By.LINK_TEXT, "group page").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
 
-    def logout(self, webdriver):
+    def logout(self, wd):
         # logout
-        webdriver.find_element(By.LINK_TEXT, "Logout").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def test_add_group(self):
-        webdriver = self.webdriver
-        self.open_home_page(webdriver)
-        self.login(webdriver, "admin", "secret")
-        self.open_groups_page(webdriver)
-        self.create_group(webdriver, Group("Jk", "Jkjkjk", "Jkjcdskjk"))
-        self.return_to_group_page(webdriver)
-        self.logout(webdriver)
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group("Jk", "Jkjkjk", "Jkjcdskjk"))
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
     def test_add_empty_group(self):
-        webdriver = self.webdriver
-        self.open_home_page(webdriver)
-        self.login(webdriver, "admin", "secret")
-        self.open_groups_page(webdriver)
-        self.create_group(webdriver, Group("", "", ""))
-        self.return_to_group_page(webdriver)
-        self.logout(webdriver)
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group("", "", ""))
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
-        self.webdriver.quit()
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
